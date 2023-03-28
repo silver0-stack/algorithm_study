2caedf49d2c9a0b507880dc439ab38c07581965b
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-03-29 02:13:12 +0900

    [PGS] 가운데 글자 가져오기 / LV.1 / 25분
    https://school.programmers.co.kr/learn/courses/30/lessons/12903
    📌새로 알게된 점
     - 조건문으로 짝/홀수 가릴 때 하나를 조건문으로 작성하면 나머지는 그냥 else로 퉁쳐도 된다는 것
     - string의 특정 인덱스 하나의 값을 추출하고 싶을 때는 get() 이용!
     - get() 함수의 리턴타입이 char이어서 toString() 해줬어야 했음
     - substring(a..b) 머리 속에 각인!!

--- eunyoungchoi/가운데 글자 가져오기.kt
+++ eunyoungchoi/가운데 글자 가져오기.kt
@@ -1,5 +1,5 @@
 class Solution {
-    fun solution(s: String): String  {
+    fun solution(s: String): String {
         var len:Int=s.length
         if(len % 2 ==1) return s.get(len / 2).toString()
         else return s.substring(len/2-1..len/2)
