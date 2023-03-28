3dccbaa4218dacdc6f16285c0d8e5942f40f2268
박지영 <slpjenny@naver.com>
2023-03-28 15:53:57 +0900

    [PGS] 양꼬치/ LV.0 / 1분

---
+++ jiyoungpark/양꼬치.java
@@ -0,0 +1,15 @@
+package jiyoungpark;
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = 0;
+
+        service = n / 10;
+
+        // 10인분 -> -2000원
+        answer = (n * 12000 + k * 2000) - (service * 2000);
+
+        return answer;
+    }
+}
