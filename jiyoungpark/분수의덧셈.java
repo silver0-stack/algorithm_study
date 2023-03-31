9b39094cabe88db143cf7d18020c1786d46c2687
박지영 <slpjenny@naver.com>
2023-03-31 23:15:45 +0900

    [PGS] 분수의 덧셈/LV.0 / 30분

---
+++ jiyoungpark/분수의덧셈.java
@@ -0,0 +1,40 @@
+package jiyoungpark;
+
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+
+        int gop = denom1 * denom2;
+
+        // 최대공약수
+        int gcd = gcd(denom1, denom2);
+
+        // 최대공배수
+        int lcm = gop / gcd;
+
+        if (denom1 != lcm) {
+            numer1 = numer1 * (lcm / denom1);
+        }
+
+        if (denom2 != lcm) {
+            numer2 = numer2 * (lcm / denom2);
+        }
+
+        int bunja = numer1 + numer2;
+
+        int[] answer = { bunja, lcm };
+
+        int max = gcd(bunja, lcm);
+
+        answer[0] /= max;
+        answer[1] /= max;
+
+        return answer;
+    }
+
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+
+        return gcd(b, a % b);
+    }
+}
